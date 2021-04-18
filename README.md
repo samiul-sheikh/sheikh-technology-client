25e27b1530eee62b3db4e0c37138b9ae400c137b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-18 22:43:34 +0600

    devide dashboard for user and admin

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 - React JS
 - React Router
 - FontAwesomeIcon
+---
+## :link: [See live project here](https://sheikh-technologies.web.app/)
+
+---
+### `Project Features`
+1. professional looks UI design.
+2. set up private route, without Login user can not take a service.
+3. when users click take service, route will be changed dynamically and go to order service page.
+4. From Dashboard admin option users can add a new service or delete a service and add an admin. all information are store in database.
+5. Full Responsive website for web and mobile devices.
+6. Complete all requirements and added extra feature project section.
--- src/Components/Dashboard/Sidebar/Sidebar.js
+++ src/Components/Dashboard/Sidebar/Sidebar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt, faGripHorizontal, faUserPlus, faShoppingBag, faPlusCircle, faBorderAll } from '@fortawesome/free-solid-svg-icons';
+import { UserContext } from '../../../App';
 
 const Sidebar = () => {
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        fetch('https://sb-technologies.herokuapp.com//isAdmin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsAdmin(data));
+    }, [])
+
     return (
-        <div className="sidebar d-flex flex-column justify-content-between col-md-auto py-5 px-4" style={{height:"100vh"}}>
+        <div className="sidebar d-flex flex-column justify-content-between col-md-auto py-5 px-4" style={{ height: "100vh" }}>
             <ul className="list-unstyled">
                 <li>
                     <Link to="/dashboard" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span> 
+                        <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span>
                     </Link>
                 </li>
                 <li>
@@ -23,21 +38,23 @@ const Sidebar = () => {
                         <FontAwesomeIcon icon={faPlusCircle} /> <span>Add Review</span>
                     </Link>
                 </li>
-                <li>
-                    <Link to="/addAdmin" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faUserPlus} /> <span>Make Admin</span> 
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/addService" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faPlusCircle} /> <span>Add Service</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/services" className="text-white text-decoration-none">
-                      <FontAwesomeIcon icon={faBorderAll} /> <span>Manage Services</span>
-                    </Link>
-                </li>
+                {isAdmin && <div>
+                    <li>
+                        <Link to="/addAdmin" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faUserPlus} /> <span>Make Admin</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/addService" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faPlusCircle} /> <span>Add Service</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/services" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faBorderAll} /> <span>Manage Services</span>
+                        </Link>
+                    </li>
+                </div>}
             </ul>
             <div>
                 <Link to="/" className="text-white text-decoration-none"><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
